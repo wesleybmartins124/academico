fe22615ee061d2294a63197cfa33186c575c8211
francisco <franads@gmail.com>
2021-09-09 12:34:00 -0300

    ajustes referente ao turma a

--- src/main/java/br/sp/gov/etec/academico/controller/AlunoController.java
+++ src/main/java/br/sp/gov/etec/academico/controller/AlunoController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.sp.gov.etec.academico.dto.AlunoEntity;
@@ -15,36 +16,45 @@ import br.sp.gov.etec.academico.repository.AlunoRepository;
 
 
 @Controller
+@RequestMapping("aluno")
 public class AlunoController {
 	
 	@Autowired
 	AlunoRepository repository;
 	
 	
-	@GetMapping("alunos")
+	@GetMapping("listar")
 	public ModelAndView listarAlunos() {	
 		
 		ModelAndView view = new ModelAndView("ListaAlunos");
 		
 		List<AlunoEntity> listaAlunos = repository.findAll();	
 		
-		view.addObject(new AlunoEntity());
-		
 		view.addObject("alunos", listaAlunos);		
 		
 		return view;
 	}
 	
-	@PostMapping("salvar-aluno")
+	@GetMapping("cadastrar")
+	public ModelAndView cadastrarAluno() {	
+		
+		ModelAndView view = new ModelAndView("CadastroAluno");		
+		
+		view.addObject(new AlunoEntity());			
+		
+		return view;
+	}
+	
+	@PostMapping("salvar")
 	public String salvarAluno(AlunoEntity aluno) {
 		
 		repository.save(aluno);
 		
-		return "redirect:/alunos";
+		return "redirect:/aluno/cadastrar";
 		
 	}
 	
-	@GetMapping("/aluno/editar/{id}")
+	@GetMapping("/editar/{id}")
 	public String editarAluno(@PathVariable Long id, Model model) {		
 		AlunoEntity alunoEntity = repository.findById(id).get();		
 		model.addAttribute(alunoEntity);		
@@ -52,10 +62,10 @@ public class AlunoController {
 		
 	}
 	
-	@GetMapping("/aluno/excluir/{id}")
+	@GetMapping("/excluir/{id}")
 	public String excluirAluno(@PathVariable Long id) {		
 			repository.deleteById(id);			
-		return "redirect:/alunos";		
+		return "redirect:/aluno/listar";		
 		
 	}
 }
